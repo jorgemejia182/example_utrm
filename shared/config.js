dc3f8edf9be281ebec67d34a396202a1ea11a723
jorgemejia182 <jorge_mejia@outlook.com>
2024-02-21 20:43:50 -0500

    fix

--- shared/config.js
+++ shared/config.js
@@ -2,3 +2,4 @@ console.log("Hola mis panas, esto es una configuración");
 
 console.log(JSON.stringify("Pollo asado", null, 2))
 console.log(JSON.stringify("Pollo frito", null, 2))
+console.debug('test bug');
